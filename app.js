47dab56e74ec56b4ec0c5cd7ed3b76a9483afb51
Malediwy2002 <marcinkapusta120@gmail.com>
2026-09-13 16:00:53 +0200

    Pierwsza werja gotowa, podstawowe funkcje

--- app.js
+++ app.js
@@ -38,10 +38,11 @@ const list = [
 ];
 
 const input = document.querySelector('#searchInput');
-const container = document.getElementById("grid-container");
+const container = document.querySelector("#grid-container");
+const up_btn = document.querySelector('#up_btn');
 
 
-
+/* Stara funckja
 function LoadList(value) {
     container.innerHTML="";
     if(!value){list.map( (item) => {
@@ -49,7 +50,7 @@ function LoadList(value) {
                 <img src='img/${item.foto}.jpg'/>
                 <div>
                     <p>${item.nazwa}</p>
-                    <p>${item.cena}</p>
+                    <p>${item.cena}zł</p>
                 </div>
             </div>`;
                 container.insertAdjacentHTML("beforeend", html);
@@ -63,21 +64,60 @@ function LoadList(value) {
                                     <img src='img/${item.foto}.jpg'/>
                                     <div>
                                         <p>${item.nazwa}</p>
-                                        <p>${item.cena}</p>
+                                        <p>${item.cena}zł</p>
                                     </div>
                                 </div>`;
                 container.insertAdjacentHTML("beforeend", html);};
                     })
                 }
+}*/
+
+function LoadList(list)
+{
+    if(list.length != 0)
+    {
+        container.innerHTML="";
+        list.map( (item) => {
+            let html = 
+            `<div class="poz">
+                <img src="img/${item.foto}.jpg" alt="${item.foto}">
+                <div>
+                    <p>${item.nazwa}</p>
+                    <p>${item.cena}</p>
+                </div>
+            </div>`;
+            container.insertAdjacentHTML("beforeend", html);
+        })
+    }else container.innerHTML = "<h1>Brak wyników!</h1>";
 }
 
 
 
-
 document.addEventListener("DOMContentLoaded", () => {
-    LoadList();
+    LoadList(list);
     input.addEventListener("input", () => {
-    if(input.value)LoadList(input.value);
-        else LoadList();
+    if(input.value)
+        {
+            let wynik = list.filter( (item) => {
+                if(item.nazwa.includes(input.value))return item;
+            })
+            LoadList(wynik);
+        }
+        else LoadList(list);
+})
+document.querySelector('#input_link').addEventListener("click", () => {
+    document.querySelector('#searchInput').focus();//Chciałem dać fous po przeniesieniu ,lecz nie zadziałąl w tym przypadku
+    //alert('Powinno być');
+
 })
 });
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 250)
+    {
+        up_btn.style = "display:flex";    
+    }else up_btn.style = "display:none";  
+})
+
+up_btn.addEventListener("click", () => {
+    window.scrollTo(0,0);
+})
